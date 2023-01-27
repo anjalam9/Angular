a472a1f7803cc8727ed611f720520aab273d839c
Anjala <madala.anjala@gmail.com>
2023-01-27 14:19:02 -0800

    style changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'analog-clock';
+export class AppComponent implements OnInit {
+  hourDegrees: number | undefined;
+  minuteDegrees: number | undefined;
+  secondDegrees: number | undefined;
+
+  constructor() { }
+
+  ngOnInit() {
+    setInterval(() => {
+      this.updateTime();
+    }, 1000);
+  }
+
+  updateTime() {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    this.hourDegrees = (hours / 12) * 360 + (minutes / 60) * 30;
+    this.minuteDegrees = (minutes / 60) * 360 + (seconds / 60) * 6;
+    this.secondDegrees = (seconds / 60) * 360;
+  }
 }
